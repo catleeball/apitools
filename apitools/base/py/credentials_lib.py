5e2e2c31caef13febb23a9b3dbdb6e89edaa4b52
Cat Lee Ball 🎷🐛 <cball@google.com>
2019-06-04 12:03:08 -0700

    Decode utf8 before json.dumps

--- apitools/base/py/credentials_lib.py
+++ apitools/base/py/credentials_lib.py
@@ -320,7 +320,7 @@ class GceAssertionCredentials(gce.AppAssertionCredentials):
         # Credentials metadata dict.
         creds = {'scopes': sorted(list(scopes)),
                  'svc_acct_name': self.__service_account_name}
-        creds_str = json.dumps(creds)
+        creds_str = json.dumps(creds.decode('utf-8'))
         cache_file = _MultiProcessCacheFile(cache_filename)
         try:
             cache_file.LockedWrite(creds_str)
